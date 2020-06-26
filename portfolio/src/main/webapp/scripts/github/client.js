a89e9ee338224f0642d1da1b423ac039eb96a4b2
Ernesto Garcia <ernestognw@gmail.com>
2020-06-25 20:28:55 -0500

    style(prettier): Homologate prettier styling
    Prettier was added, but it was not applied to every file.
    Github client was added.

--- portfolio/src/main/webapp/scripts/github/client.js
+++ portfolio/src/main/webapp/scripts/github/client.js
@@ -1,16 +1,16 @@
 const baseURL = "https://api.github.com";
 
 const github = {
-    get: async path => {
-        const data = await fetch(`${baseURL}${path}`, {
-            headers: {
-                Accept: "application/vnd.github.v3+json"
-            }
-        });
+  get: async (path) => {
+    const data = await fetch(`${baseURL}${path}`, {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
 
-        return data.json();
-    }
-    // TO DO: Add another methods to github
+    return data.json();
+  },
+  // TO DO: Add another methods to github
 };
 
-export { github }
+export { github };
