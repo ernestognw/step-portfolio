4255bb8a6406fe234bce9e4b4ff72c81171651fa
ernestognw <ernestognw@gmail.com>
2020-07-01 23:37:42 -0500

    feature(comments): Add in memory comments handling
    Added a new doPost method to comments servlet, so I can add new comments to the page. It is still needed to set them at the frontend.

--- portfolio/src/main/java/com/google/sps/servlets/CommentsController.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentsController.java
@@ -24,6 +24,10 @@ import java.util.ArrayList;
 import com.google.gson.Gson;
 import java.util.Date;
 import com.google.sps.models.Comment;
+import java.util.Scanner;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 @WebServlet("/comments")
 public class CommentsController extends HttpServlet {
@@ -32,7 +36,7 @@ public class CommentsController extends HttpServlet {
   @Override
   public void init() {
     comments = new ArrayList<>();
-    for(Integer i = 0; i < 5; i++){
+    for (Integer i = 0; i < 5; i++) {
       comments.add(0, new Comment("Foo", "Bar", new Date()));
     }
   }
@@ -44,6 +48,59 @@ public class CommentsController extends HttpServlet {
     Gson gson = new Gson();
     String data = gson.toJson(comments);
 
-    response.getWriter().println(data);  
+    response.getWriter().println(data);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json;");
+
+    String body = getBody(request);
+    JSONObject json = new JSONObject();
+    try {
+      JSONParser parser = new JSONParser();
+      json = (JSONObject) parser.parse(body);
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+
+    String username = getAttribute(json, "username", "Anonymous");
+    String comment = getAttribute(json, "comment", "...");
+    Date createdAt = new Date();
+
+    comments.add(0, new Comment(username, comment, createdAt));
+
+    Gson gson = new Gson();
+    String data = gson.toJson(comments);
+
+    response.getWriter().println(data);
+  }
+
+  private String getBody(HttpServletRequest request) throws IOException {
+    if ("POST".equalsIgnoreCase(request.getMethod())) {
+      Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+      scanner.useDelimiter("\\A");
+
+      String body = "";
+
+      if (scanner.hasNext()) {
+        body = scanner.next();
+      }
+
+      scanner.close();
+
+      return body;
+    }
+
+    return "";
+  }
+
+  private String getAttribute(JSONObject json, String attribute, String defaultValue) {
+    String value = (String) json.get(attribute);
+    if (value == null) {
+      return defaultValue;
+    }
+
+    return value;
   }
 }
