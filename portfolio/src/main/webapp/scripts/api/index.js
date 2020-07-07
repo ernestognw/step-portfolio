f52ea2e5f47d929423ceedd1f207dc6dc310b64a
ernestognw <ernestognw@gmail.com>
2020-07-07 17:50:41 -0500

    fix(index.js): Change the order of imports
    The order of function imports from comments api were changed, along with the exports to comply with the styleguide

--- portfolio/src/main/webapp/scripts/api/index.js
+++ portfolio/src/main/webapp/scripts/api/index.js
@@ -1,21 +1,6 @@
 import { api } from './client.js';
 import { getCommentTemplate } from './utils.js';
 
-const updateComments = async () => {
-  const comments = await api.get('/comments');
-
-  const container = document.getElementById('comments');
-
-  for (let comment of comments) {
-    const { createdAt, username, comment: text } = comment;
-
-    container.insertAdjacentHTML(
-      'beforeend',
-      getCommentTemplate(username, text, createdAt)
-    );
-  }
-};
-
 const addComment = async newComment => {
   const { username, comment, createdAt } = newComment;
   const submitButton = document.getElementById('submit-post');
@@ -41,4 +26,19 @@ const addComment = async newComment => {
   submitButton.classList.remove('is-loading');
 };
 
-export { updateComments, addComment };
+const updateComments = async () => {
+  const comments = await api.get('/comments');
+
+  const container = document.getElementById('comments');
+
+  for (let comment of comments) {
+    const { createdAt, username, comment: text } = comment;
+
+    container.insertAdjacentHTML(
+      'beforeend',
+      getCommentTemplate(username, text, createdAt)
+    );
+  }
+};
+
+export { addComment, updateComments };
